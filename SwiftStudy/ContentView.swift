c68e326431f20437af18c7cf7a1eb06f4177bf76
yufffff <y.fukai.0325@gmail.com>
2021-07-04 12:13:49 +0900

    commit

---
+++ SwiftStudy/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftStudy
+//
+//  Created by 深井裕貴 on 2021/07/04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftStudy/SwiftStudyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftStudyApp.swift
+//  SwiftStudy
+//
+//  Created by 深井裕貴 on 2021/07/04.
+//
+
+import SwiftUI
+
+@main
+struct SwiftStudyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
